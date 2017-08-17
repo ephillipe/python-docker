8fedad30f6c1b7d591187afd10328a1fd1719a4b
Erick Ponce <erickponceleao@gmail.com>
2017-08-17 14:33:33 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ APP_SCRIPT=${APP_SCRIPT:-application.py}
 APP_REQUIREMENTS=${APP_REQUIREMENTS:-requirements.txt}
 APP_PORT=${APP_PORT:-8080}
 CURRENT_DIR=$(pwd)
-SOMAXCONN=${SOMAXCONN:-100}
+SOMAXCONN=${SOMAXCONN}
 
 echo "Current directory: " $CURRENT_DIR
 echo "Application script: " $APP_SCRIPT
@@ -13,6 +13,9 @@ echo "Application requirements: " $APP_REQUIREMENTS
 echo "Application port: " $APP_PORT
 export PYTHONPATH="${PYTHONPATH}":$CURRENT_DIR
 echo "Current PYTHONPATH:" $PYTHONPATH
+
+echo "Preparing Python environment..."
+pip install --upgrade -r $APP_REQUIREMENTS
 
 echo "Starting cron..."
 service cron start
